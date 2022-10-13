2460ae24c3d68c3a4a20c5d2eb4fbfdd90ccb6e4
徐徳成 <ekcarter@syr.edu>
2022-10-12 17:02:16 -0700

    12thOct

--- HW2BinaryTree/BinaryTree/src/LowestCommonAncestorofaBinaryTree/TreeNode.java
+++ HW2BinaryTree/BinaryTree/src/LowestCommonAncestorofaBinaryTree/TreeNode.java
@@ -17,7 +17,7 @@ import java.util.Set;
 
     Map<Integer, TreeNode> parent = new HashMap<Integer, TreeNode>();
     Set<Integer> visited = new HashSet<Integer>();
-
+    //Store all nodes and their right and left children nodes.
     public void dfs(TreeNode root) {
         if (root.left != null) {
             parent.put(root.left.val, root);
@@ -31,16 +31,22 @@ import java.util.Set;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         dfs(root);
+        //this while loop will put p.val into the hashset.then parent.get() will return
+        //p's parent node. Finally, we will get all parent node.
         while (p != null) {
             visited.add(p.val);
             p = parent.get(p.val);
         }
+        //if the level which q located in is shallower p(q level is 5,p level is 10),which means q is common nodes (visited contain q already)
+        //if the q level is deeper than p level, visited hashset does not contain q levels.
+        //Then we need to use parent.get() to bring get q's parent. until visited hash contain q.
         while (q != null) {
             if (visited.contains(q.val)) {
                 return q;
             }
             q = parent.get(q.val);
         }
+        //after we finish two while loop, if we still do not return, we return null.
         return null;
     }
 }
